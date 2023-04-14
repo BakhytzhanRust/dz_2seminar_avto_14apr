022a651fe5caed2b54b52cbd638b2e65483fcf76
Bakhytzhan <bahitgan_2001b@mail.ru>
2023-04-14 14:30:27 +0300

    exercise10

---
+++ dz10_cop/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.Write("Enter three-numbered count ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+if (n3 < 100 || n3 > 1000)
+{
+ Console.WriteLine("That's not three-numbered count, please, try again");
+}
+else
+{
+int n2 = n3 % 100 / 10;
+Console.WriteLine($" {n3} -> {n2}");
+}
---
+++ dz15/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+Console.Write("Enter number from 1 to 7, meant the day of the week ");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x < 1 || x >7)
+{
+  Console.Write("You made mistake, that cannot mean the day of the week");
+}
+else
+  if ( 1 <= x && x <= 5)
+  {
+    Console.Write($"{x} -> no");
+  }
+  else
+  {
+    Console.Write($"{x} -> yes");
+  }
